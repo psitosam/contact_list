e882fe2de4fc21382edd62921d61e935167cb1c5
Alex Psitos <apsitos08@gmail.com>
2022-05-22 10:04:08 -0400

    Feat: phone passes validation and relationship tests

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -1,8 +1,8 @@
 class Phone < ApplicationRecord
 
   belongs_to :contact
-  # validates_presence_of :number
-  # validates_presence_of :phone_type
+  validates_presence_of :number
+  validates_presence_of :phone_type
   enum phone_type: {
     home: 0,
     work: 1,
