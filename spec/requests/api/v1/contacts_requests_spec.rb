4d6eac18ece69dd08256d965b4fb53a1149f8856
Alex Psitos <apsitos08@gmail.com>
2022-05-26 13:44:27 -0400

    Functionality for all user stories complete

--- spec/requests/api/v1/contacts_requests_spec.rb
+++ spec/requests/api/v1/contacts_requests_spec.rb
@@ -189,38 +189,47 @@ RSpec.describe 'the contacts API' do
 
   context 'call list' do
     it 'returns an array of contacts that have home phone numbers' do
-      contact_1 = create :contact
-      phone_1 = create :phone, { phone_type: "mobile", contact_id: contact_1.id }
+
+      contact_1 = Contact.create(first_name: "India", middle_name: "Corwin", last_name: "Lynch", street: "612 Cyndy Coves", city: "Howellton", state: "Oklahoma", zip: "41265-1557", phone: [], email: "ernest@simonis-wolf.org")
+      phone_1 = Phone.create(number: "937-262-3861", phone_type: "mobile", contact_id: contact_1.id)
       #contact_1 has only a mobile number, non-select expected
 
-      contact_2 = create :contact
+      contact_2 = Contact.create(first_name: "Thea", middle_name: "Fisher", last_name: "Kuhn", street: "926 Kimberly Road", city: "North Billy", state: "Maine", zip: "03740-3342", phone: [], email: "ericka_beahan@hermann-tromp.io")
       phone_2 = create :phone, { contact_id: contact_2.id }
       phone_6 = create :phone, { phone_type: "mobile", contact_id: contact_2.id }
       # contact_2 has both a home number and a mobile number
 
-      contact_3 = create :contact
+      contact_3 = Contact.create(first_name: "Sherita", middle_name: "Graham", last_name: "Treutel", street: "6669 Williamson Square", city: "Port Erna", state: "New Mexico", zip: "60239-2314", phone: [], email: "nobuko.pfeffer@buckridge.com")
       phone_3 = create :phone, { phone_type: "work", contact_id: contact_3.id }
       # contact_3 has only a work number, non-select expected
 
-      contact_4 = create :contact
+      contact_4 = Contact.create(first_name: "Janeen", middle_name: "McCullough", last_name: "Quigley", street: "9166 McKenzie Square", city: "Friesenborough", state: "Iowa", zip: "00050-1101", phone: [], email: "leigh@okeefe-franecki.com")
       phone_4 = create :phone, { contact_id: contact_4.id }
       phone_7 = create :phone, { phone_type: "work", contact_id: contact_4.id }
       # contact_4 has both home and work numbers
 
-      contact_5 = create :contact
+      contact_5 = Contact.create(first_name: "Long", middle_name: "Breitenberg", last_name: "Wolff", street: "224 Grant Islands", city: "Port Trentburgh", state: "New Hampshire", zip: "62350-2289", phone: [], email: "ronny@cole.name")
       phone_5 = create :phone, { contact_id: contact_5.id }
       # contact_5 has only a home number
-      # Total expected contacts: 3
+      # Expecting an Array of Contacts with 3 total Contacts included. Sorting by last name will put "Thea Kuhn" first, followed by "Janeen Quigley" and "Long Wolff".
 
       get "/api/v1/contacts/call-list"
 
       parsed = JSON.parse(response.body, symbolize_names: true)
       contacts = parsed[:data]
-require 'pry'; binding.pry
+
       expect(response).to be_successful
       expect(response.status).to eq(200)
       expect(contacts.class).to eq(Array)
-      expect(contacts.length).to eq(5)
+      expect(contacts.length).to eq(3)
+      expect(contacts[0][:attributes][:name][:last]).to eq("Kuhn")
+      expect(contacts[0][:attributes][:phone].class).to eq(Array)
+      expect(contacts[0][:attributes][:phone].first[:phone_type]).to eq("home")
+      expect(contacts[1][:attributes][:name][:last]).to eq("Quigley")
+      expect(contacts[1][:attributes][:phone].first[:phone_type]).to eq("home")
+      expect(contacts[2][:attributes][:name][:last]).to eq("Wolff")
+      expect(contacts[2][:attributes][:phone].first[:phone_type]).to eq("home")
+
     end
 
   end
